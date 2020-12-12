c833d3c9436f695163104f344cae31bab0bd8e4b
Kimm <hyunsung.kim@zeals.co.jp>
2020-12-12 23:15:44 +0900

    create fetch fuctions

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,10 +1,14 @@
+import Axios from 'axios';
 import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
+import { getEnvironment } from './enviornment';
 
 // Initial State
 
 const initialState = {
   transactions: [],
+  error: null,
+  loading: true,
 };
 
 // Global context
@@ -14,27 +18,73 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
-  // Actions
-  function addTransaction(transaction) {
-    dispatch({
-      type: 'ADD_TRANSACTION',
-      payload: transaction,
-    });
+  async function getTransactions() {
+    try {
+      const res = await Axios.get(`${getEnvironment()}/api/transactions`);
+      dispatch({
+        type: 'GET_TRANSACTION',
+        payload: res.data.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: 'ERROR_TRANSACTION',
+        payload: error.response.data.error,
+      });
+    }
   }
 
-  function deleteTransaction(id) {
-    dispatch({
-      type: 'DELETE_TRANSACTION',
-      payload: id,
-    });
+  // Actions
+
+  async function addTransaction(transaction) {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await Axios.post(
+        `${getEnvironment()}/api/transactions`,
+        transaction,
+        config
+      );
+      dispatch({
+        type: 'ADD_TRANSACTION',
+        payload: res.data.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: 'ERROR_TRANSACTION',
+        payload: error.response.data.error,
+      });
+    }
+  }
+
+  async function deleteTransaction(id) {
+    try {
+      await Axios.delete(`${getEnvironment()}/api/transactions/${id}`);
+
+      dispatch({
+        type: 'DELETE_TRANSACTION',
+        payload: id,
+      });
+    } catch (error) {
+      dispatch({
+        type: 'ERROR_TRANSACTION',
+        payload: error.response.data.error,
+      });
+    }
   }
 
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        getTransactions,
         addTransaction,
         deleteTransaction,
+        error: state.error,
+        loading: state.loading,
       }}
     >
       {children}
