c5c8502461d549b4487c98a0a4f374b81f218100
Kimm <hyunsung.kim@zeals.co.jp>
2020-12-12 23:17:29 +0900

    implement CURD in front end

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -3,18 +3,18 @@ import { GlobalContext } from '../context/GlobalState';
 
 export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
-
-  const sign = transaction.amount < 0 ? '－' : '＋';
+  const isExpense = transaction.amount < 0;
+  const sign = isExpense ? '－' : '＋';
 
   return (
-    <li className={transaction.amount < 0 ? 'out' : 'in'}>
-      {transaction.text}{' '}
+    <li className={isExpense ? 'out' : 'in'}>
+      {transaction.text}
       <span>
-        {sign}¥{Math.abs(transaction.amount)}
+        {sign}¥{Math.abs(transaction.amount).toLocaleString()}
       </span>
       <button
         className="remove-button"
-        onClick={() => deleteTransaction(transaction.id)}
+        onClick={() => deleteTransaction(transaction._id)}
       >
         x
       </button>
